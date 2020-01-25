e01cdcaa3cb844215796497b826d1c64b3b9fb46
jcampbell11245 <47761573+jcampbell11245@users.noreply.github.com>
2020-01-24 23:29:35 -0500

    Finished board class and started GUI

--- src/Board.java
+++ src/Board.java
@@ -1,50 +1,144 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Board 
 {
 	private int[][] board;
+	private boolean[][] clicked;
+	private boolean dead;
+	private int numOpened;
+	private int tiles;
+	private int bombs;
 	
 	/**
-	 * Randomly generates the board based on the size and where and how the user clicked
-	 * @param widith
+	 * Sets up the board object
+	 */
+	public Board()
+	{
+		dead = false;
+		numOpened = 0;
+	}
+	
+	/**
+	 * Randomly generates the board based on the size, number of bombs and where 
+	 * and how the user clicked
 	 * @param height
+	 * @param widith
 	 * @param row
 	 * @param col
 	 * @param click
 	 */
-	public void generateBoard(int width, int height, int row, int col, String click)
+	public void generateBoard(int height, int width, int bombs, int row, int col, boolean[][] marked)
 	{
+		tiles = height * width;
+		this.bombs = bombs;
+		
 		board = new int[height][width];
+		clicked = new boolean[height][width];
+		
+		//Places bombs at unique random spots on the board (other than the spot
+		//where the user clicked)
+		Random rand = new Random();
+		int r1;
+		int r2;
+		ArrayList<String> takenSpots = new ArrayList<String>();
+		for(int i = 0; i < bombs; i++) {
+			r1 = rand.nextInt(height);
+			r2 = rand.nextInt(width);
+			while(takenSpots.contains(r1 + " " + r2) || r1 == row && r2 == col) {
+				r1 = rand.nextInt(height);
+				r2 = rand.nextInt(width);
+			}
+			
+			board[r1][r2] = 9;
+			takenSpots.add(r1 + " " + r2);
+		}
+		
+		//Counts and stores the number of bombs in each squares area
+		int numBombs;
+		for(int r = 0; r < height; r++) {
+			for (int c = 0; c < width; c++) {
+				if(board[r][c] != 9) {
+					numBombs = 0;
+					
+					if(r > 0 && c > 0 && board[r - 1][c - 1] == 9) {numBombs++;}
+					if(r > 0 && board[r - 1][c] == 9) {numBombs++;}
+					if(r > 0 && c < width - 1 && board[r - 1][c + 1] == 9) {numBombs++;}
+					if(c > 0 && board[r][c - 1] == 9) {numBombs++;}
+					if(c < width - 1 && board[r][c + 1] == 9) {numBombs++;}
+					if(r < height - 1 && c > 0 && board[r + 1][c - 1] == 9) {numBombs++;}
+					if(r < height - 1 && board[r + 1][c] == 9) {numBombs++;}
+					if(r < height - 1 && c < width - 1 && board[r + 1][c + 1] == 9) {numBombs++;}
+					
+					board[r][c] = numBombs;
+				}
+			}
+		}
+		
+		updateBoard(row, col, marked);
 	}
 
 	/**
-	 * Updates the board based on where and how the user clicked
+	 * Updates the board based on where the user clicked
 	 * @param row
 	 * @param col
 	 * @param click
 	 */
-	public void updateBoard(int row, int col, String click)
+	public void updateBoard(int row, int col, boolean[][] marked)
 	{
+		if(!marked[row][col]) {
+			numOpened++;
+			clicked[row][col] = true;
+			
+			int state = board[row][col];
 		
+			if(state != 9) {
+				if(state == 0) {
+					if(row > 0 && col > 0 && !clicked[row - 1][col - 1]) {updateBoard(row - 1, col - 1, marked);}
+					if(row > 0 && !clicked[row - 1][col]) {updateBoard(row - 1, col, marked);}
+					if(row > 0 && col < board[0].length - 1 && !clicked[row - 1][col + 1]) {updateBoard(row - 1, col + 1, marked);}
+					if(col > 0 && !clicked[row][col - 1]) {updateBoard(row, col - 1, marked);}
+					if(col < board[0].length - 1 && !clicked[row][col + 1]) {updateBoard(row, col + 1, marked);}
+					if(row < board.length - 1 && col > 0 && !clicked[row + 1][col - 1]) {updateBoard(row + 1, col - 1, marked);}
+					if(row < board.length - 1 && !clicked[row + 1][col]) {updateBoard(row + 1, col, marked);}
+					if(row < board.length - 1 && col < board[0].length - 1 && !clicked[row + 1][col + 1]) {updateBoard(row + 1, col + 1, marked);}
+				}
+			}
+			else {
+				dead = true;
+			}
+		}
 	}
 
 	/**
 	 * Returns the 2d array for the board
-	 * @return
+	 * @return 
 	 */
 	public int[][] getBoard()
 	{
 		return board;
 	}
+	
+	/**
+	 * Returns the 2d array for the spaces on the board that have been clicked
+	 * @return
+	 */
+	public boolean[][] getClicked()
+	{
+		return clicked;
+	}
 
 	/**
-	 * Returns the state of the board, 0 if game still progressing, 1 if player blew up, 2 if all tiles cleared
+	 * Returns the state of the board, 0 if game still progressing, 1 if player
+	 * blew up, 2 if all tiles cleared
 	 * @return
 	 */
 	public int getBoardState()
 	{
-		int state = 0;
+		if(dead) {return 1;}
 		
-	
+		if(numOpened == tiles - bombs) {return 2;}
 		
-		return state;
+		return 0;
 	}
 }
--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -1,12 +1,24 @@
+import java.util.ArrayList;
+
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class Minesweeper extends Application
 {
 	Stage window;
+	Board board;
+	BorderPane gameLayout;
+	boolean[][] marked;
 	
 	/**
 	 * Sets up the GUI by launching the javafx window
@@ -30,8 +42,8 @@ public class Minesweeper extends Application
 		window.setResizable(false);
 		window.setWidth(700);
 		window.setHeight(850);
-		window.setScene(titleScene());
-		window.setTitle("Checkers"); 
+		window.setScene(gameScene());
+		window.setTitle("Minesweeper"); 
 		window.show();
 	}
 	
@@ -53,9 +65,22 @@ public class Minesweeper extends Application
 	 */
 	public Scene gameScene()
 	{
-		Pane layout = new Pane();
+		gameLayout = new BorderPane();
+		marked = new boolean[10][10];
 		
-		Scene scene = new Scene(layout, 700, 750);
+		board = new Board();
+//		int[][] boardP = board.getBoard();
+//		for(int r = 0; r < 16; r++) {
+//			for(int c = 0; c < 30; c++) {
+//				System.out.print(boardP[r][c] + " ");
+//			}
+//			System.out.println();
+//		}
+		
+		gameLayout.setCenter(getBoardLayout(10, 10, 10, true));
+		gameLayout.setPadding(new Insets(20, 20, 20, 20));
+		
+		Scene scene = new Scene(gameLayout, 700, 750);
 		return scene;
 	}
 
@@ -67,6 +92,7 @@ public class Minesweeper extends Application
 	{
 		VBox layout = new VBox();
 		
+		
 		Scene scene = new Scene(layout, 700, 750);
 		return scene;
 	}
@@ -75,11 +101,76 @@ public class Minesweeper extends Application
 	 * Generates a visual for the board based on the corresponding 2d array
 	 * @return VBox
 	 */
-	public VBox getBoardLayout()
+	public VBox getBoardLayout(int height, int width, int bombs, boolean firstClick)
 	{
-		VBox board = new VBox();
+		boolean[][] clickedArr = board.getClicked();
+		ArrayList<Button> buttons = new ArrayList<Button>();
+		ArrayList<Label> labels = new ArrayList<Label>();
 		
-		return board;
+		int[][] boardArr;
+		if(!firstClick) {
+			boardArr = board.getBoard();
+		}
+		else {
+			boardArr = new int[height][width];
+		}
+		
+		HBox[] rows = new HBox[boardArr.length];
+		for(int i = 0; i < rows.length; i++) {
+			rows[i] = new HBox();
+		}
+		
+		VBox layout = new VBox();
+		for(int i = 0; i < rows.length; i++) {
+			layout.getChildren().add(rows[i]);
+		}
+		
+		for(int r = 0; r < boardArr.length; r++) {
+			for(int c = 0; c < boardArr[0].length; c++) {
+				if(!firstClick && clickedArr[r][c]) {
+					labels.add(new Label());
+					labels.get(labels.size() - 1).setText(boardArr[r][c] + "");
+					labels.get(labels.size() - 1).setTextAlignment(TextAlignment.CENTER);
+					labels.get(labels.size() - 1).setMinSize(660/boardArr[0].length, 660/boardArr.length);
+					rows[r].getChildren().add(labels.get(labels.size() - 1));
+				}
+				else {
+					final int row = r;
+					final int col = c;
+					buttons.add(new Button());
+					Button b = buttons.get(buttons.size() - 1);
+					b.setMinSize(660/boardArr[0].length, 660/boardArr.length);
+					b.setTextAlignment(TextAlignment.CENTER);
+					if(marked[row][col]) {
+						b.setText("F");
+					}
+					b.setOnMouseClicked(e -> {
+						if(e.getButton() == MouseButton.PRIMARY) {
+							if(firstClick) {
+								board.generateBoard(height, width, bombs, row, col, marked);
+							}
+							else {
+								board.updateBoard(row, col, marked);
+							}
+							gameLayout.setCenter(getBoardLayout(height, width, bombs, false));
+						}
+						else if (e.getButton() == MouseButton.SECONDARY) {
+							if(b.getText() == "") {
+								b.setText("F");
+								marked[row][col] = true;
+							}
+							else {
+								b.setText("");
+								marked[row][col] = false;
+							}
+						}
+					});
+					rows[r].getChildren().add(buttons.get(buttons.size() - 1));
+				}
+			}
+		}
+		
+		return layout;
 	}
 
 }
